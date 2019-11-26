d3b0b50fbeb571f7bc8732b7a9ab31ac2decb33b
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2019-11-26 20:13:31 +0530

    add images

--- photography/nameall.py
+++ photography/nameall.py
@@ -1,6 +1,8 @@
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, splitext
 from mimetypes import guess_type
-files = [f for f in listdir() if isfile(f) and guess_type(f)[0]!=None and guess_type(f)[0].find("image")!=-1]
+filetypes = [".JPG", ".PNG", ".GIF", ".WEBP", ".TIFF", ".PSD", ".RAW", ".BMP", ".HEIF", ".INDD", ".JPEG 2000", ".CR2", ".SVG", ".AI", ".PDF", ".JPEG"]
+files = [f for f in listdir() if isfile(f) and splitext(f)[1].upper() in filetypes]
 f = open("imagelist.txt", "w")
 f.write(";".join(files))
+print(len(files), " images found.")
